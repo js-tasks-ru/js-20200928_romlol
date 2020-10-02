f7e89569b7ae0da40ab6db29ffc589fa6d4eabb7
Roman Smirnov <rsmirnov3a@gmail.com>
2020-10-02 11:47:45 +0300

    Решение задач темы: Основы JS. Примитивные типы данных

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,50 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const resultArr = Array.from(arr);
+    if (param === 'asc') {
+        return resultArr.sort(compareString);
+    } else {
+        return resultArr.sort(compareString).reverse();
+    }
+}
 
+export function compareString(a, b) {
+    for (let i = 0; i < a.length; i++) {
+        const compChar = compareChar(a[i], b[i]);
+        if (compChar !== 0) {
+            return compChar;
+        }
+    }
+    return 0;
+}
+export function compareChar(a, b) {
+  //Заглавную выше прописной
+  if (a.toLowerCase() === b && a.charCodeAt(0) !== b.charCodeAt(0)) {
+    return -1;
+  }
+  // далее сравниваем тока прописные
+  const codeA = a.toLowerCase().charCodeAt(0);
+  const codeB = b.toLowerCase().charCodeAt(0);
+  //обработка ё
+  if (codeA === 1105) {
+    if (1078 <= codeB && codeB <= 1103) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+  // Поднимаем русские буквы вверх по приоритету сортировки
+  // а = символы до русских букв
+  // && b = 'а-я' или 'ё'
+  if (codeA <= 1025 && (1072 <= codeB && codeB <= 1103 || codeB === 1105)) {
+    return 1;
+  }
+  if (codeA < codeB) {
+    return -1;
+  }
+  if (codeA > codeB) {
+    return 1;
+  }
+  return 0;
 }
--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+    const result = {};
+    fields.forEach(field => {
+        if (obj[field] !== undefined) {
+            result[field] = obj[field]; 
+        }
+    });
+    return result;
 };
