ca35d6620119ab45fc45fe19bcdfa3668834e61e
Roman Smirnov <rsmirnov3a@gmail.com>
2020-10-19 12:23:45 +0300

    DoubleSlider

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -17,24 +17,76 @@ export default class DoubleSlider {
     const element = document.createElement('div');
     element.innerHTML = this.getTemplate;
     this.element = element.firstElementChild;
+    document.body.appendChild(this.element);
+    this.leftSlider = this.element.querySelector('.range-slider__thumb-left');
+    this.rightSlider = this.element.querySelector('.range-slider__thumb-right');
+    this.progress = this.element.querySelector('.range-slider__progress');
+    this.recalcPositions();
+    this.addEventListener();
+  }
+
+  addEventListener() {
     this.element.addEventListener('range-select', this.rangeSelect.bind(this));
+    window.addEventListener('resize', this.recalcPositions.bind(this));
+
     this.element.addEventListener('mousedown', this.mouseDown.bind(this));
     this.element.addEventListener('mousemove', this.mouseMove.bind(this));
     this.element.addEventListener('mouseup', this.mouseUp.bind(this));
-    document.body.appendChild(this.element);
+  }
+
+  recalcPositions(event) {
+    this.minPosition = this.progress.getBoundingClientRect().left;
+    this.maxPosition = this.progress.getBoundingClientRect().right;
+
+    this.leftSliderPosition = this.minPosition + this.getSliderLength * this.left / 100;
+    this.rightSliderPosition = this.maxPosition - this.getSliderLength * this.right / 100;
   }
 
   rangeSelect(event) {
     //rangeSelect
   }
+
+  get getSliderLength() {
+    return this.maxPosition - this.minPosition;
+  }
+
   mouseDown(event) {
-    //mouseDown
+    if (event.target === this.leftSlider) {
+      this.minNewPosition = this.minPosition;
+      this.maxNewPosition = this.rightSliderPosition;
+      this.currentMovableElement = event.target;
+    }
+    if (event.target === this.rightSlider) {
+      this.minNewPosition = this.leftSliderPosition;
+      this.maxNewPosition = this.maxPosition;
+      this.currentMovableElement = event.target;
+    }
   }
+
   mouseMove(event) {
-    //mouseMove
+    if (!this.currentMovableElement) {
+      return;
+    }
+    this.newPosition = null;
+
+    if (event.offsetX - this.minPosition) {
+
+    }
+    if (this.minNewPosition < event.offsetX && event.offsetX < this.maxNewPosition) {
+      switch (this.currentMovableElement) {
+      case this.leftSlider:
+        console.log(event.offsetX + this.minPosition, this.getSliderLength);
+        this.currentMovableElement.style.left = ((event.offsetX + this.minPosition) / this.getSliderLength * 100 - 100) + '%';
+        break;
+      case this.rightSlider:
+        this.currentMovableElement.style.right = (event.offsetX / this.maxPosition * 100) + '%';
+        break;
+      }
+    }
   }
+
   mouseUp(event) {
-    //mouseUp
+    this.currentMovableElement = null;
   }
 
   get getTemplate() {
